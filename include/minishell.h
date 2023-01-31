e4daa2bc3cefacbbe1c137d94bd69e802320c102
Luis Laens <lulaens@3D5.42angouleme.fr>
2023-01-31 10:55:33 +0100

    readline

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tcazenav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 09:58:47 by tcazenav          #+#    #+#             */
-/*   Updated: 2023/01/31 10:49:22 by lulaens          ###   ########.fr       */
+/*   Updated: 2023/01/31 10:53:23 by lulaens          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
-# include "libft/libft.h"
+# include "../libft/libft.h"
 
 char	*current_path(void);
 
--- src/main.c
+++ src/main.c
@@ -6,17 +6,25 @@
 /*   By: tcazenav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 09:57:57 by tcazenav          #+#    #+#             */
-/*   Updated: 2023/01/31 10:18:55 by tcazenav         ###   ########.fr       */
+/*   Updated: 2023/01/31 10:54:53 by lulaens          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <readline/readline.h>
 
 int	main(void)
 {
 	char	*path;
+	char	*line;
 
+	line = NULL;
 	path = current_path();
+	while (1)
+	{
+		line = readline("$>");
+		printf("%s", line);
+	}
 	free(path);
 	return (0);
 }
