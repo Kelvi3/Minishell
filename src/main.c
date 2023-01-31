6a43da778c78ed52c563a6b2a812e7208ddcfc52
Vusk42 <mathis.rudloff@gmail.com>
2023-01-31 11:45:58 +0100

    BIGGGGGGGGGGGGGGGGGGGGGGGGGG

--- src/main.c
+++ src/main.c
@@ -13,16 +13,26 @@
 #include "../include/minishell.h"
 #include <readline/readline.h>
 
+static char	*get_prompt(void)
+{
+	char	*prompt;
+	prompt = ft_strdup("\e[1;36mminishell$>\x1B[0m");
+	return (prompt);
+}
+
 int	main(void)
 {
 	char	*path;
 	char	*line;
+	char	*prompt;
 
 	line = NULL;
 	path = current_path();
 	while (1)
 	{
-		line = readline("$>");
+		prompt = get_prompt();
+		line = readline(prompt);
+		free(prompt);
 		printf("%s", line);
 	}
 	free(path);
