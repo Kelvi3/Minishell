4edee60878b12a3c1f2e0c8d4bfc0e127fb8ec69
Theo Cazenave-coupet <tcazenav@3A5.42angouleme.fr>
2023-02-01 08:55:18 +0100

    include

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tcazenav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 09:58:47 by tcazenav          #+#    #+#             */
-/*   Updated: 2023/01/31 10:53:23 by lulaens          ###   ########.fr       */
+/*   Updated: 2023/02/01 08:35:19 by tcazenav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,16 @@
 # include <signal.h>
 # include "../libft/libft.h"
 
-char	*current_path(void);
+typedef struct s_element
+{
+	char	**cmd;	
+}				t_element;
+
+char		*current_path(void);
+void		condition_prompt(char *line);
+
+//parse cmd
+t_element	*parse_cmd(char *line);
+char		**parse_cmd_bis(t_element arr, char *line);
 
 #endif
